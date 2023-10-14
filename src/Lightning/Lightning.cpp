4d0ce091cea6db669ddbb2b4ba4dadafd4e4837c
M-Alcantar <alcantar.mauricio@cetys.edu.mx>
2023-10-13 21:49:05 -0700

    something or whatevr im dying
    AAAAAAAAAAA

--- src/Lightning/Lightning.cpp
+++ src/Lightning/Lightning.cpp
@@ -1,13 +1,17 @@
 #include "Lightning.h"
-
 using namespace std;
 
-Field::Field(void){
+// FIELD CLASS
+Field::Field(){
     this->len = 40;
     this->wid = 40;
 }
+<<<<<<< Updated upstream:src/Lightning/Lightning.cpp
 Field::~Field(void){
     cout << "Te voy a castear." << endl;
+=======
+Field::~Field(){
+>>>>>>> Stashed changes:Lightning/Lightning.cpp
 }
 
 void Field::setLen(int len){ this->len = len; }
@@ -34,3 +38,32 @@ void Field::show(void){             //only for testing purposes
         cout << endl;
     }
 }
+
+// POINT CLASS
+Point::Point(){
+    this->isLight = false;
+    this->prevX = 0;
+    this->prevY = 0;
+}
+Point::~Point(){
+}
+
+void Point::setIsLight(bool light){ this->isLight = light; }
+void Point::setPrevX(int x){ this->prevX = x; }
+void Point::setPrevY(int y){ this->prevY = y; }
+bool Point::getIsLight(void){ return isLight; }
+int Point::getPrevX(void){ return prevX; }
+int Point::getPrevY(void){ return prevY; }
+
+// LIGHTNING CLASS
+Lightning::Lightning(){
+    this->leeway = 0.2;
+    this-> branch = 0.05;
+}
+Lightning::~Lightning(){
+}
+
+void Lightning::setLeeway(float l){ this->leeway = l; }
+void Lightning::setBranch(float b){ this->branch = b; }
+float Lightning::getLeeway(void){ return leeway; }
+float Lightning::getBranch(void){ return branch; }
--- src/Lightning/Lightning.h
+++ src/Lightning/Lightning.h
@@ -13,8 +13,8 @@ private:
     float grid[40][40];
 
 public:
-    Field(void);
-    ~Field(void);
+    Field();
+    ~Field();
 
     void setLen(int);
     void setWid(int);
@@ -23,6 +23,40 @@ public:
 
     void randomize(void);
     void show(void);
+};
+
+class Point{
+private:
+    bool isLight;
+    int prevX;
+    int prevY;
+
+public:
+    Point();
+    ~Point();
+
+    void setIsLight(bool);
+    void setPrevX(int);
+    void setPrevY(int);
+    bool getIsLight(void);
+    int getPrevX(void);
+    int getPrevY(void);
+};
+
+class Lightning{
+private:
+    Point lightGrid[40][40];
+    float leeway;
+    float branch;
+
+public:
+    Lightning();
+    ~Lightning();
+
+    void setLeeway(float);
+    void setBranch(float);
+    float getLeeway(void);
+    float getBranch(void);
 };
 
 #include "Lightning.cpp"
