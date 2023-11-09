7c71fa7c79ac6d00328ff23da61bd695a9020e82
HakkinDavid <desr11@hotmail.com>
2023-11-08 19:11:48 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -260,7 +260,7 @@ int main(){
         thunder_data << L"Masa electrónica total: " << scientific << setprecision(std::numeric_limits<long double>::digits10 + 1) << storm.getElectronicMass() << "kg" << endl;
         thunder_data << L"Ajuste de mínimos cuadrados: x = " << fixed << setprecision(4) << direction[1] << " " << (direction[0] > 0 ? "+" : "-") << " " << fixed << setprecision(4) << abs(direction[0]) << "y" << endl;
         thunder_data << L"Coeficiente de correlación (R): " << fixed << setprecision(4) << direction[2] << endl;
-        thunder_data << L"Coeficiente de determinación (R^2): " << fixed << setprecision(4) << direction[2]*direction[2] << endl;
+        thunder_data << L"Coeficiente de determinación (R²): " << fixed << setprecision(4) << direction[2]*direction[2] << endl;
         thunder_data << L"Dimensión fractal: " << fixed << setprecision(4) << storm.getFracs()->back() << endl;
         text.setString(thunder_data.str());
         dim_text_bg.setSize(sf::Vector2f(text.getLocalBounds().getSize().x + 10, text.getLocalBounds().getSize().y + 10));
