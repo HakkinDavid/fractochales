b1ca7f11a1ce8f904b583ed9538307d219e55a61
M-Alcantar <alcantar.mauricio@cetys.edu.mx>
2024-03-10 20:12:20 -0700

    Seriously why is it doing that

--- includes/Lightning/Lightning.cpp
+++ includes/Lightning/Lightning.cpp
@@ -84,6 +84,7 @@ void Lightning::traverse(int x, int y, int z, int prevxyz[3], bool tag){
     while(x >= 0 && x < hei && y >= 0 && y < wid && z >= 0 && z < dep){
         int diff[3] = { x - prevxyz[0], y - prevxyz[1], z - prevxyz[2] }; 
         vector<array<int, 3>> neighbors;
+        array<int, 3> minVal, min2Val;
         float neighborVal[10] = {0};
         int key = 9, min = 9, min2 = 9;
 
@@ -176,6 +177,7 @@ void Lightning::traverse(int x, int y, int z, int prevxyz[3], bool tag){
             return;
         } // End of branch
 
+
         // Check for branching opportunities
         for(int i=0; i<key; i++){
             if(i != min && neighborVal[i] >= neighborVal[min]-branch){
@@ -183,22 +185,29 @@ void Lightning::traverse(int x, int y, int z, int prevxyz[3], bool tag){
             }
         }
 
+        // Wipe neighbors
+        minVal[0] = neighbors[min][0]; minVal[1] = neighbors[min][1]; minVal[2] = neighbors[min][2]; 
+        if((piece != 3 && min2 < 9) || (piece == 3 && min2 < 10)){
+            min2Val[0] = neighbors[min2][0]; min2Val[1] = neighbors[min2][1]; min2Val[2] = neighbors[min2][2];
+        }
+        neighbors.clear();
+
         // Next Step
         prevxyz[0] = x; prevxyz[1] = y; prevxyz[2] = z;
         canonVertices.push_back({x, y, z});
-        canonVertices.push_back({neighbors[min][0], neighbors[min][1], neighbors[min][2]});
-        std::cout << prevxyz[0] << " " << prevxyz[1] << " " << prevxyz[2] << " => " << neighbors[min][0] << " " << neighbors[min][1] << " " << neighbors[min][2] << std::endl;
+        canonVertices.push_back(minVal);
+        //std::cout << prevxyz[0] << " " << prevxyz[1] << " " << prevxyz[2] << " => " << minVal[0] << " " << minVal[1] << " " << minVal[2] << std::endl;
         if((piece != 3 && min2 == 9) || (piece == 3 && min2 == 10)){ // No branching
-            x = neighbors[min][0];
-            y = neighbors[min][1];
-            z = neighbors[min][2];
+            x = minVal[0];
+            y = minVal[1];
+            z = minVal[2];
         }
         else{                                                        // Yes branching >:)
             canonVertices.push_back({x, y, z});
-            canonVertices.push_back({neighbors[min2][0], neighbors[min2][1], neighbors[min2][2]});
-            traverse(neighbors[min][0], neighbors[min][1], neighbors[min][2], prevxyz);
-            std::cout << prevxyz[0] << " " << prevxyz[1] << " " << prevxyz[2] << " => " << neighbors[min2][0] << " " << neighbors[min2][1] << " " << neighbors[min2][2] << std::endl;
-            traverse(neighbors[min2][0], neighbors[min2][1], neighbors[min2][2], prevxyz);
+            canonVertices.push_back(min2Val);
+            traverse(minVal[0], minVal[1], minVal[2], prevxyz);
+            //std::cout << prevxyz[0] << " " << prevxyz[1] << " " << prevxyz[2] << " => " << min2Val[0] << " " << min2Val[1] << " " << min2Val[2] << std::endl;
+            traverse(min2Val[0], min2Val[1], min2Val[2], prevxyz);
             return;
         }
 
@@ -237,7 +246,7 @@ void Lightning::superTraverse(){
 
     // Traversing loop
     do{
-        std::cout << "mimama es trABis" << std::endl;
+        //std::cout << "mimama es trABis" << std::endl;
         // Find lowest and closest to center lightning point
         x = -1; y = -1; z = -1;
         for(int i = hei-1; i >= 0; i--){
